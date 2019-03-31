7080578d9c4610da76720a5d4f2ab4cbb7f1ca0c
bigbluey <joseph.yon@gmail.com>
2019-03-30 20:54:35 -0700

    MongoDB and Flask Application Code Update

--- flask_app/app.py
+++ flask_app/app.py
@@ -21,8 +21,20 @@ mongo = PyMongo(app)
 #################################################
 # Flask Routes
 #################################################
-# Root Route to Query MongoDB & Pass Mars Data Into HTML Template to Display the Data
+# Root Route to Query MongoDB & Pass Mars Data Into HTML Template: index.html to Display Data
 @app.route("/")
 def index():
     mars = mongo.db.mars.find_one()
     return render_template("index.html", mars=mars)
+
+# Scrape Route to Import `scrape_mars.py` Script & Call `scrape` Function
+@app.route("/scrape")
+def scrapper():
+    mars = mongo.db.mars
+    mars_data = scrape_mars.scrape_all()
+    mars.update({}, mars_data, upsert=True)
+    return "Scraping Successful"
+
+# Define Main Behavior
+if __name__ == "__main__":
+    app.run()
