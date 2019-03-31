4ab68d6fde49261b0dc16f9173894658987dcd0c
bigbluey <joseph.yon@gmail.com>
2019-03-30 20:28:30 -0700

    MongoDB and Flask Application Code

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,4 +1,6 @@
+#################################################
 # MongoDB and Flask Application
+#################################################
 
 # Dependencies and Setup
 from flask import Flask, render_template
@@ -15,3 +17,12 @@ app = Flask(__name__)
 #################################################
 app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
 mongo = PyMongo(app)
+
+#################################################
+# Flask Routes
+#################################################
+# Root Route to Query MongoDB & Pass Mars Data Into HTML Template to Display the Data
+@app.route("/")
+def index():
+    mars = mongo.db.mars.find_one()
+    return render_template("index.html", mars=mars)
